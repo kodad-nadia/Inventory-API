44153d1956bab30c2f12c2cd91df7887bc243f08
kodad-nadia <106157374+kodad-nadia@users.noreply.github.com>
2024-06-16 22:52:07 +0200

    added docker

---
+++ database.py
@@ -0,0 +1,26 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# Récupération de l'URL de la base de données
+DATABASE_URL = os.getenv('DATABASE_URL', "mysql+pymysql://root:@localhost:3306/Item-docker-api")
+
+# Créer un moteur de base de données afin de gérer la connexion
+engine = create_engine(DATABASE_URL)
+
+# Créer une session locale pour interagir avec la base de données
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Créer une base déclarative
+Base = declarative_base()
+
+# Définir une fonction pour obtenir une session de base de données
+def get_db():
+    db = SessionLocal()
+    try:
+        # La session de base de données est ouverte et est utilisée dans la requête
+        yield db
+    finally:
+        # La session de base de données est fermée après la fin de la requête
+        db.close()
